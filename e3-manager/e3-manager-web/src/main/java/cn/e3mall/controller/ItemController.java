e109d044a1d1e8569fdba9a2fa3f5a833d883422
gang.zhu <gang.zhu@beibei.com>
2018-08-13 19:32:27 +0800

    测试

--- e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
+++ e3-manager/e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -2,7 +2,6 @@ package cn.e3mall.controller;
 
 import cn.e3mall.pojo.TbItem;
 import cn.e3mall.service.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
 
     @Resource
     private ItemService itemService;
 
-    @RequestMapping("/item/{itemId}")
+    @RequestMapping("/{itemId}")
     @ResponseBody
-    private TbItem getItemById(@PathVariable Long itemId) {
-        TbItem tbItem = itemService.getItemById(itemId);
-        return tbItem;
+//    private TbItem getItemById(@PathVariable Long itemId) {
+    public String getItemById(@PathVariable(value = "id")  Long id) {
+//        TbItem tbItem = itemService.getItemById(itemId);
+        return "item";
     }
 }
